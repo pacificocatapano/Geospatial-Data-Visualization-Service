939507a040c9dd6bea72413a127f24325709c033
simone-dotolo <simonedotolo@libero.it>
2023-07-01 13:18:58 +0200

    ubxparser

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import pyubx2
 import pandas as pd
 
-file_path = 'COM10___115200_221206_130405.ubx'
-stream = open(file_path, 'rb')
-ubr = pyubx2.UBXReader(stream)
+class UBXParser():
 
-LLH = []
-STATUS = []
+    def __init__(self,
+                 stream_path):
+        self.stream_path = stream_path
+    
+    def parse_all(self):
+        LLH = []
+        STATUS = []
 
-for _, parsed in ubr:
-    if parsed != None:
-        if parsed.identity == 'NAV-HPPOSLLH':
-            LLH.append([parsed.iTOW, parsed.lon, parsed.lat, parsed.height])
-        if parsed.identity == 'NAV-STATUS':
-            pass
+        with open(self.stream_path, 'rb') as stream:
+            ubr = pyubx2.UBXReader(stream)
+            
+            for _, parsed in ubr:
+                if parsed != None:
+                    if parsed.identity == 'NAV-HPPOSLLH':
+                        LLH.append([parsed.iTOW, parsed.lon, parsed.lat, parsed.height])
+                    if parsed.identity == 'NAV-STATUS':
+                        pass
+        
+        LLH = pd.DataFrame(LLH,
+                           columns=['Timestamp', 'Longitude', 'Latitude', 'Height'])
+        STATUS = pd.DataFrame(STATUS)
 
-LLH = pd.DataFrame(LLH,
-                   columns = ['Timestamp', 'Longitude', 'Latitude', 'Height'])
+        return LLH, STATUS
 
+parser = UBXParser(stream_path='../COM10___115200_221206_130405.ubx')
+LLH, STATUS = parser.parse_all()
 print(LLH)
