f9f0d51f9c338bc430b075731f79b670e0374792
simone-dotolo <simonedotolo@libero.it>
2023-07-03 16:31:26 +0200

    parrrrrsed

--- bozza_front-end.py
+++ bozza_front-end.py
@@ -11,8 +11,6 @@ import pandas as pd
 import streamlit as st 
 import os
 
-
-
 st.set_page_config(layout="wide")
 
 
@@ -21,6 +19,7 @@ st.title("Interfaccia di visualizzazione dati geospaziali \U0001F30D")
 
 df_llh = pd.read_csv("./provaUBXllh.csv", sep = ';')
 df_status = pd.read_csv("./provaUBXstatus.csv", sep = ';')
+df_ecef = pd.read_csv("./provaUBXecef.csv", sep = ';') 
 
 ### Filter sidebar
 st.sidebar.header("Filters")
@@ -37,12 +36,26 @@ timestamp_selected = st.sidebar.slider(
 time_filter_lower = df_llh['iTOW'] >= timestamp_selected[0] 
 time_filter_upper = df_llh['iTOW'] <= timestamp_selected[1]
 
-df_filtered = df_llh[time_filter_lower &  time_filter_upper]   
+df_llh_filtered = df_llh[time_filter_lower &  time_filter_upper]   
+df_ecef_filtered = df_ecef[time_filter_lower &  time_filter_upper]   
+df_status_filtered = df_status[time_filter_lower &  time_filter_upper]   
 
 
 ### Dataframe
-st.write('LLH')
-st.dataframe(df_filtered, use_container_width = True)
+if df_llh_filtered.shape[0] != 0:
+    st.write('LLH')
+    st.dataframe(df_llh_filtered,
+                 use_container_width=True,
+                 hide_index=True)
 
-st.write('STATUS')
-st.dataframe(df_status, use_container_width = True)
+if df_status.shape[0] != 0:
+    st.write('STATUS')
+    st.dataframe(df_status_filtered,
+                 use_container_width=True,
+                 hide_index=True)
+
+if df_ecef.shape[0] != 0:
+    st.write('ECEF')
+    st.dataframe(df_ecef_filtered,
+                 use_container_width=True,
+                 hide_index=True)
