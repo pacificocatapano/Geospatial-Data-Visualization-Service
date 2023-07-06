eacf7b9966c3b3004e096b12c699423f39d6eb1b
Salvatore-Moggio <sasymoggio24@gmail.com>
2023-07-06 19:30:23 +0200

    Diagrammi di sequenza raffinati

--- client.py
+++ client.py
@@ -10,13 +10,14 @@ import requests
 # URL del server per l'upload del file
 url = 'http://100.103.0.167:5000/upload'
 
-# Path del file da inviare
-file_path = 'file.txt'
-
-# Esegui la richiesta POST
-with open(file_path, 'rb') as file:
-    files = {'file': file}
-    response = requests.post(url, files=files)
-
-# Stampa la risposta
-print(response.text)
+def inviaFile(fileUBX):
+    # Path del file da inviare
+    file_path = fileUBX
+    
+    # Esegui la richiesta POST
+    with open(file_path, 'rb') as file:
+        files = {'file': file}
+        response = requests.post(url, files=files)
+    
+    # Stampa la risposta
+    print(response.text)
