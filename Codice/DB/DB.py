e2be6ec5718411e4de00cb8b93e9aa8919f068c9
simone-dotolo <84937474+simone-dotolo@users.noreply.github.com>
2023-07-18 10:18:09 +0200

    Update DB.py

--- Codice/DB/DB.py
+++ Codice/DB/DB.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def initDB():
     connection = sqlite3.connect('Codice/DB/sqlite.db')
-    with open('./DB/sqlite.sql') as f:
+    with open('Codice/DB/sqlite.sql') as f:
         connection.executescript(f.read())
     connection.commit()
     connection.close()
