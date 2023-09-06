842e590c54292f56b0386b5f0f14b1c17550471e
pacificocatapano <pacifico.catapano@gmail.com>
2023-09-06 17:58:44 +0200

    added screen

--- Codice/DB/DB.py
+++ Codice/DB/DB.py
@@ -2,8 +2,8 @@ import sqlite3
 
 def initDB():
     connection = sqlite3.connect('./DB/sqlite.db')
-    cursor = connection.cursor()
-    cursor.execute(f'DELETE FROM Utenti;')
+    #cursor = connection.cursor()
+    #cursor.execute(f'DELETE FROM Utenti;')
     with open('./DB/sqlite.sql') as f:
         connection.executescript(f.read())
     connection.commit()
